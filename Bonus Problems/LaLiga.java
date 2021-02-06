52a9f4c3da183820885f71519e18748651409723
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-02-06 12:24:52 +0530

    Create LaLiga.java
    This is Super - Sunday for football fans. It is the final night. It is Barcelona vs Real Madrid. Moreover, the talking point of this year's La Liga is Sevilla becoming the last team to qualify for the playoffs with just 12 points.
    
    The question is if 'N' teams are playing in the La Liga, what is the minimum number of points at which a team may qualify for a playoff?
    Note: 1. You have to work around a solution in the premises of the format, where a winning team is awarded 2 points and the losing team is awarded 0 points. 4 teams qualify for the playoffs, after the league stage where each team plays all the other teams once.
    2. We have to assume all the matches arrive at a result. (no draw)

---
+++ Bonus Problems/LaLiga.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+	public static void main (String[] args) {
+    // Take input as given in the input format 
+	// And accordingly print the output as in output format.
+	
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int min = 2 * (n - 4);
+        System.out.println(min);
+    }
+}
