87db942099b2dd306f81878f97ebeff1426166d8
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-02-18 10:56:34 +0530

    Create KLargestElements.java
    You are given an integer k and an array of integers that contain numbers in random order. Write a program to find k largest numbers from the given array. You need to save them in an array and return it.
    Time complexity should be O(nlogk) and space complexity should be not more than O(k).
    
    The order of elements in the output is not important.
    
    Input Format :
    Line 1: Size of the array (n)
    Line 2: Array elements (separated by space)
    Line 3: Integer k

---
+++ Priority Queue/KLargestElements.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    public static ArrayList<Integer> kLargest(int input[], int k) {
+        /* Your class should be named Solution
+		* Don't write main().
+		* Don't read input, it is passed as function argument.
+		* Return output and don't print it.
+		* Taking input and printing output is handled automatically.
+		*/
+        if(input.length <= k) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int i = 0; i < k; i++) {
+                ans.add(input[i]);
+            }
+
+            return ans;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < k; i++) {
+            pq.add(input[i]);
+        }
+
+        for(int i = k; i < input.length; i++) {
+            if(pq.peek() < input[i]) {
+                pq.poll();
+                pq.add(input[i]);
+            }
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while(!pq.isEmpty()) {
+            ans.add(pq.poll());
+        }
+
+        return ans;
+    }
+}
