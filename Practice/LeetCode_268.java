042164b7fed86debb5fcb5372b73ad438e59796a
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-06-23 22:17:18 +0530

    Create LeetCode_268.java

---
+++ Practice/LeetCode_268.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        if(nums[nums.length - 1] != nums.length){
+            return nums.length;
+        }
+        
+        if(nums[0] != 0){
+            return 0;
+        }
+        
+        for(int i = 1; i < nums.length; i++){
+            int expected = nums[i - 1] + 1;
+            if(nums[i] != expected){
+                return expected;
+            }
+        }
+        
+        return -1;
+        
+    }
+}
