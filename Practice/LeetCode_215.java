4b4db0e8da1e128460ba270959f013bdc3135e34
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-07-04 19:18:35 +0530

    Create LeetCode_215.java

---
+++ Practice/LeetCode_215.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        
+        if(n < k){
+            return -1;
+        }
+        
+        if(n == k){
+            Arrays.sort(nums);
+            return nums[k - 1];
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < k; i++){
+            pq.add(nums[i]);
+        }
+        
+        for(int i = k; i < n; i++){
+            if(pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        
+        return pq.peek();
+    }
+}
