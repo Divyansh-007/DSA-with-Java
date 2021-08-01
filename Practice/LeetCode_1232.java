8648249bfc3ff867d09106ffd0e25324bc55e352
Divyansh Jaiswal <61491200+Divyansh-007@users.noreply.github.com>
2021-08-01 10:40:45 +0530

    Create LeetCode_1232.java

---
+++ Practice/LeetCode_1232.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+         if(coordinates.length == 2){
+            return true;
+        }
+        
+        int dy = (coordinates[1][1]-coordinates[0][1]);
+        int dx = (coordinates[1][0]-coordinates[0][0]);
+        
+        for (int i = 2; i < coordinates.length; i++){
+            if(dy * (coordinates[i][0] - coordinates[1][0]) != dx * (coordinates[i][1] - coordinates[1][1])){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
